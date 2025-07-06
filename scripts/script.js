30fd81af82cb70d4a9f3dfca8c962d8b91e6eeec
Tanmoy Santra <tanmoysantra2812004@gmail.com>
2025-07-07 00:49:47 +0530

    Select and log a random quiz question
    Updated getRandomQuestion to select a random question from the chosen category and log it, instead of logging the entire category object.

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,11 @@
 let quizCategory = "JavaScript";
 
 const getRandomQuestion = () => {
-    const catagoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase());
-    console.log(catagoryQuestions);
+    const catagoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions;
+    
+    const randomQuestion = catagoryQuestions[Math.floor(Math.random() * catagoryQuestions.length)];
+
+    console.log(randomQuestion);
 }
 
 getRandomQuestion();
